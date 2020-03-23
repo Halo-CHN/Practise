0c32d125f5124aa44af193d8e604860f19994a3c
Halo CHN <halochn@anyonedeMacBook-Pro.local>
2020-03-23 19:37:47 +0800

    增加设计模式练习module

---
+++ pattern/src/main/java/io/github/halochn/pattern/flyweight/Flyweight.kt
@@ -0,0 +1,7 @@
+package io.github.halochn.pattern.flyweight
+
+open abstract class Flyweight constructor(private var name: String) {
+    var intrinsicState: String? = null
+
+    abstract fun getExtrinsicState(state: String)
+}
