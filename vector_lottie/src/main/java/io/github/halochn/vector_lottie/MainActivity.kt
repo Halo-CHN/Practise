35f3b276a90797cd0f94d9060d65fd119d3f4bb5
张伟光 <zhangweiguang@xin.com>
2020-07-01 11:09:57 +0800

    improvement(package name): rename to io.github.halochn

---
+++ vector_lottie/src/main/java/io/github/halochn/vector_lottie/MainActivity.kt
@@ -0,0 +1,33 @@
+package io.github.halochn.vector_lottie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.constraintlayout.motion.widget.MotionLayout
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        motionLayout?.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+                //TODO("Not yet implemented")
+            }
+
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+                //TODO("Not yet implemented")
+            }
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+                //TODO("Not yet implemented")
+                lottieAnimationView.progress = p3
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                //TODO("Not yet implemented")
+            }
+        })
+    }
+}
