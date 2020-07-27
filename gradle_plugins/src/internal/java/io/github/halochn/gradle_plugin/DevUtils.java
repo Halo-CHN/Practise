e3e21249ca9f9621e521516851cc9f8afe2aef63
张伟光 <zhangweiguang@xin.com>
2020-07-27 11:46:35 +0800

    feat(gradle plugin): do it

---
+++ gradle_plugins/src/internal/java/io/github/halochn/gradle_plugin/DevUtils.java
@@ -0,0 +1,16 @@
+package io.github.halochn.gradle_plugin;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+import android.view.ViewGroup;
+
+class DevUtils {
+    public static void setUI(Activity activity) {
+        ViewGroup decorView = (ViewGroup) activity.getWindow().getDecorView();
+        View badge = new View(activity);
+        badge.setAlpha(0.5F);
+        badge.setBackgroundColor(Color.RED);
+        decorView.addView(badge, -1, 150);
+    }
+}
