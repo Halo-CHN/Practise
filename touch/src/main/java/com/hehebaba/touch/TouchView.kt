cb26342b5a3541de8ad7e29b2d4a0a851130714f
张伟光 <zhangweiguang@xin.com>
2021-03-08 18:29:45 +0800

    feat(触摸反馈): demo

--- touch/src/main/java/com/hehebaba/touch/TouchView.kt
+++ touch/src/main/java/com/hehebaba/touch/TouchView.kt
@@ -1,4 +1,16 @@
 package com.hehebaba.touch
 
-class TouchView {
+import android.content.Context
+import android.util.AttributeSet
+import android.view.ViewGroup
+
+class TouchView constructor(context: Context, attrs: AttributeSet?) : ViewGroup(context, attrs) {
+
+    override fun shouldDelayChildPressedState(): Boolean {
+        return false
+    }
+
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+
+    }
 }
